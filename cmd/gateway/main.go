6d53c9a9133a29f0db2a6418a27df15c40bdc4f3
SkyfuryX <gannonshanley@gmail.com>
2026-09-10 13:35:08 -0400

    rate limiter middleware added

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -10,6 +11,41 @@ import (
 
 	"github.com/redis/go-redis/v9"
 )
+
+func getMinuteKey(apiKey string) string {
+	now := time.Now().Unix()
+	minuteBucket := now / 60
+
+	return fmt.Sprintf("rate:%s:%d", apiKey, minuteBucket)
+}
+
+func rateLimitMiddleware(rdb *redis.Client, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		const limit int64 = 10
+		apiKey := r.Header.Get("X-API-Key")
+		if apiKey == "" {
+			http.Error(w, "Missing API Key\n", http.StatusUnauthorized)
+			return
+		}
+
+		rateKey := getMinuteKey(apiKey)
+		count, err := rdb.Incr(r.Context(), rateKey).Result()
+		if err != nil {
+			log.Printf("Error incrementing key: %v", err)
+			http.Error(w, "Internal Server Error\n", http.StatusInternalServerError)
+			return
+		}
+		if count == 1 {
+			rdb.Expire(r.Context(), rateKey, 60*time.Second)
+		}
+
+		if count > limit {
+			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	rdb := redis.NewClient(&redis.Options{
@@ -41,8 +77,8 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("GET /1", proxy)
-	mux.Handle("GET /2", proxy)
+	mux.Handle("GET /1", rateLimitMiddleware(rdb, proxy))
+	mux.Handle("GET /2", rateLimitMiddleware(rdb, proxy))
 
 	const port = "8080"
 	srv := &http.Server{
